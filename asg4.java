3f681317aa1c60db636fc2be32b77347b047c7e6
bmanisha12 <b.manisha59@gmail.com>
2016-03-15 15:25:22 +0530

    Added files via upload

---
+++ asg4.java
@@ -0,0 +1,31 @@
+
+public class asg4 {
+public static void main(String args[])
+{
+	int x = 5, y = 10 , ans; 
+	
+	ans= x+y*2;
+	System.out.println("\n answer = "+ans);
+	
+	ans=x-y+2;
+	System.out.println("\n answer = "+ans);
+	
+	ans=(x+y)*2;
+	System.out.println("\n answer = "+ans);
+	
+	ans=y%x;
+	System.out.println("\n answer = "+ans);
+}
+}
+/*output: 
+ 
+ answer = 25
+
+ answer = -3
+
+ answer = 30
+
+ answer = 0
+
+ 
+ */
